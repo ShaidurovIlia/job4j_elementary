cb36ac0814d9469efe325019461daed0282c081a
Admin <shaidurov.ilha@yandex.ru>
2021-07-12 15:51:48 +0300

    3.4. Заголовок в файле java [#159036]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Ilia Shaidurov");
+        System.out.println("19.10.1994");
+    }
+}
