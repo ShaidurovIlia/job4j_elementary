8b38bece62ba0b5fe48cbd87b80be8c11ac4d28e
Admin <shaidurov.ilha@yandex.ru>
2021-07-15 22:34:20 +0300

    6. Идеальный вес [#41584]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -3,13 +3,11 @@ package ru.job4j;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value/ 70f;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value/60;
-        return rsl;
+        return value/60f;
     }
 
     public static void main(String[] args) {
