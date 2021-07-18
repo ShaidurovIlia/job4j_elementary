f10902090a1f08a54199724e58bab8608cefb4f5
Admin <shaidurov.ilha@yandex.ru>
2021-07-18 23:55:57 +0300

    4.1. Аргументы [#295521]

---
+++ src/ru/job4j/calculate/Сalculator.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class Сalculator {
+    public static void plus(int first, int second){
+        int result=first+second;
+        System.out.println(result);
+    }
+    public static void main(String[]args){
+        Сalculator.plus(100,500);
+        Сalculator.plus(4,2);
+        Сalculator.plus(3,5);
+    }
+}
