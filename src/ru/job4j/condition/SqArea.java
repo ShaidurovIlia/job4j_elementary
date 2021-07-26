6de17b8a241a0017020f4f1e85486ba174cb1f4c
Admin <shaidurov.ilha@yandex.ru>
2021-07-26 18:54:59 +0300

    9.1. String [#237407]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,11 +3,11 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double h=p/2*(k+1);
-        double L=h*k;
-        return L*h;
+        double l=h*k;
+        return l*h;
     }
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k =2 , s = 2, real = " + result1);
+        double resultOne = SqArea.square(6, 2);
+        System.out.println(" p = 6, k =2 , s = 2, real = " + resultOne);
     }
 }
