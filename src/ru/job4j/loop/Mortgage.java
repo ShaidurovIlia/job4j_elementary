2f3edc718403dd7c4248aacf08eed72ab51dc667
Admin <shaidurov.ilha@yandex.ru>
2021-09-06 23:37:26 +0300

    5.7. Ипотека [#156315 #199712]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
         int year = 0;
         while (amount > 0) {
             amount = amount + (percent / 100) * amount - salary;
-            year++;
+            year = year + 1;
         }
         return year;
     }
