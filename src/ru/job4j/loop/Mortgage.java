01e23d121e2f78570b06603b60160bcc45861b54
Admin <shaidurov.ilha@yandex.ru>
2021-09-06 23:32:20 +0300

    5.7. Ипотека [#156315 #199712]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,6 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            System.out.println(year);
             amount = amount + (percent / 100) * amount - salary;
             year++;
         }
